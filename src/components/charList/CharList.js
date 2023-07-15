77d8ddf608df4f9bfccd14297954b785df6e9938
AlexKor94 <sasha1994work@gmail.com>
2023-07-16 00:04:07 +0300

    add err behaviour and get random character

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,44 +1,46 @@
 import './charList.scss';
 import abyss from '../../resources/img/abyss.jpg';
 
+//char__item_selected
+
 const CharList = () => {
     return (
         <div className="char__list">
             <ul className="char__grid">
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
                 <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
+                    <img src={abyss} alt="abyss" />
                     <div className="char__name">Abyss</div>
                 </li>
             </ul>
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,24 +1,30 @@
 import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
-
 
     state = {
-        char: {}
+        char: {},
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
 
+    componentDidMount() {
+        this.updateChar();
+    }
+
     onCharLoaded = (char) => {
-        this.setState({ char })
+        this.setState({
+            char: char,
+            loading: false
+        })
     }
 
     updateChar = () => {
@@ -26,30 +32,35 @@ class RandomChar extends Component {
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError)
+    }
+
+    onGetRandomCharacter = () => {
+        this.setState({
+            loading: true
+        });
+        this.updateChar();
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
     }
 
     render() {
-        const { char: { name, description, thumbnail, homepage, wiki } } = this.state;
-        const displayDescr = (description !== undefined && description.length > 0) ? description.slice(0, 206) + "..." : "Sorry, but the description is empty.";
+        const { char, loading, error } = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spiner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View char={char} /> : null;
+
+        // console.log(error);
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img" />
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {displayDescr}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spiner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br />
@@ -59,13 +70,39 @@ class RandomChar extends Component {
                         Or choose another one
                     </p>
                     <button className="button button__main">
-                        <div className="inner">try it</div>
+                        <div className="inner" onClick={this.onGetRandomCharacter}>try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
-                </div>
-            </div>
+                </div >
+            </div >
         )
     }
+}
+
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki } = char;
+    // const { error } = error;
+    const displayDescr = (description !== undefined && description.length > 0) ? description.slice(0, 206) + "..." : "Sorry, but the description is empty.";
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {displayDescr}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RandomChar;
