32ca161d166a5879cadcf334fad350a8a14b9619
AlexKor94 <sasha1994work@gmail.com>
2023-07-16 22:53:01 +0300

    charInfo

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 
 import './charList.scss';
@@ -12,27 +13,41 @@ class CharList extends Component {
 
     state = {
         characters: [],
-        loading: true
+        loading: true,
+        err: false
     }
 
     componentDidMount() {
         const marvelService = new MarvelService();
         marvelService.getAllCharacters().then(res => {
-            this.setState({
-                characters: res,
-                loading: false
-            })
-        });
+            this.onLoaded(res);
+        }).catch(err => this.onError());
+    }
+
+    onLoaded(res) {
+        this.setState({
+            characters: res,
+            loading: false
+        })
+    }
+
+    onError() {
+        this.setState({
+            loading: false,
+            err: true
+        })
     }
 
     render() {
-        const { characters, loading } = this.state;
+        const { characters, loading, err } = this.state;
         const spinner = loading ? <Spinner /> : null;
+        const error = err ? <ErrorMessage /> : null;
         return (
             <div className="char__list">
+                {error}
                 {spinner}
                 <ul className="char__grid">
-                    <CreateCards characters={characters} />
+                    <CreateCards characters={characters} onCharSelected={this.props.onCharSelected} />
                 </ul>
                 <button className="button button__main button__long">
                     <div className="inner">load more</div>
@@ -43,7 +58,7 @@ class CharList extends Component {
 }
 
 
-const CreateCards = ({ characters }) => {
+const CreateCards = ({ characters, onCharSelected }) => {
     const items = characters.map(character => {
 
         let imgStyle = { 'objectFit': 'cover' };
@@ -52,7 +67,7 @@ const CreateCards = ({ characters }) => {
         }
 
         return (
-            < li className="char__item" key={character.id}>
+            < li className="char__item" key={character.id} onClick={() => onCharSelected(character.id)}>
                 <img
                     src={character.thumbnail}
                     alt={character.name}
---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,14 @@
+import Component from 'react';
+
+class ErrorBoundary extends Component {
+
+  state = {
+    error: false
+  }
+
+
+
+  //   render() {
+  //     <h2> Error occured, sorry! </h2>
+  //   }
+}
